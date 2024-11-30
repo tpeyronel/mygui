8fde4720f801667ce376935ba552693efb63f8c4
Tomás Peyronel <tpeyronel@gmail.com>
2024-11-29 23:44:03 -0300

    add failing test

--- src/ui.rs
+++ src/ui.rs
@@ -1167,6 +1167,78 @@ mod tests {
                 ],
             );
         }
+
+        #[test]
+        fn box_fit_content_with_fill_parent_child_parent_with_border() {
+            test_converter(
+                128.0,
+                128.0,
+                UiNode::Box(BoxProps {
+                    modifiers: Modifiers::new()
+                        .width(Extent::FitContent)
+                        .height(Extent::FitContent)
+                        .border_thickness(BorderThickness::all(8.0))
+                        .self_alignment(Alignment::BottomLeft),
+                    children: vec![
+                        UiNode::Box(BoxProps {
+                            modifiers: Modifiers::new()
+                                .width(Extent::FillParent)
+                                .height(Extent::FillParent)
+                                .fill_color(Color::new(1.0, 0.0, 0.0, 1.0)),
+                            children: vec![],
+                        }),
+                        UiNode::Box(BoxProps {
+                            modifiers: Modifiers::new()
+                                .width(Extent::Px(8.0))
+                                .height(Extent::Px(64.0))
+                                .self_alignment(Alignment::BottomLeft),
+                            children: vec![],
+                        }),
+                        UiNode::Box(BoxProps {
+                            modifiers: Modifiers::new()
+                                .width(Extent::Px(96.0))
+                                .height(Extent::Px(8.0))
+                                .self_alignment(Alignment::TopRight),
+                            children: vec![],
+                        }),
+                    ],
+                }),
+                &[
+                    Rectangle {
+                        position: Vec2::new(0.0, 0.0),
+                        size: Vec2::new(96.0 + 16.0, 64.0 + 16.0),
+                        fill_color: Color::ZERO,
+                        border_color: Color::ZERO,
+                        border_radius: Vec4::splat(0.0),
+                        border_width: Vec4::splat(8.0),
+                    },
+                    Rectangle {
+                        position: Vec2::new(8.0, 8.0),
+                        size: Vec2::new(96.0, 64.0),
+                        fill_color: Color::new(1.0, 0.0, 0.0, 1.0),
+                        border_color: Color::ZERO,
+                        border_radius: Vec4::splat(0.0),
+                        border_width: Vec4::splat(0.0),
+                    },
+                    Rectangle {
+                        position: Vec2::new(8.0, 8.0),
+                        size: Vec2::new(8.0, 64.0),
+                        fill_color: Color::ZERO,
+                        border_color: Color::ZERO,
+                        border_radius: Vec4::splat(0.0),
+                        border_width: Vec4::splat(0.0),
+                    },
+                    Rectangle {
+                        position: Vec2::new(8.0, 128.0 - 64.0 - 8.0 - 8.0),
+                        size: Vec2::new(96.0, 8.0),
+                        fill_color: Color::ZERO,
+                        border_color: Color::ZERO,
+                        border_radius: Vec4::splat(0.0),
+                        border_width: Vec4::splat(0.0),
+                    },
+                ],
+            );
+        }
     }
 
     mod columns {
