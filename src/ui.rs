9a627a0f1bb68626ac18000ee88d170794bca565
Tomás Peyronel <tpeyronel@gmail.com>
2024-11-30 14:25:40 -0300

    add failing padding rounding test

--- src/ui.rs
+++ src/ui.rs
@@ -1799,4 +1799,41 @@ mod tests {
             );
         }
     }
+
+    mod rounding {
+        use super::*;
+
+        #[test]
+        fn padding_rounding() {
+            test_converter(
+                32.0,
+                32.0,
+                UiNode::Box(BoxProps {
+                    modifiers: Modifiers::new().padding(Padding::all(3.5)), // Should all be rounded to 4.0
+                    children: vec![UiNode::Box(BoxProps {
+                        modifiers: Modifiers::new().fill_color(Color::new(1.0, 0.0, 0.0, 1.0)),
+                        children: vec![],
+                    })],
+                }),
+                &[
+                    Rectangle {
+                        position: Vec2::new(0.0, 0.0),
+                        size: Vec2::new(32.0, 32.0),
+                        fill_color: Color::ZERO,
+                        border_color: Color::ZERO,
+                        border_radius: Vec4::ZERO,
+                        border_width: Vec4::ZERO,
+                    },
+                    Rectangle {
+                        position: Vec2::new(4.0, 4.0),
+                        size: Vec2::new(32.0 - 8.0, 32.0 - 8.0),
+                        fill_color: Color::new(1.0, 0.0, 0.0, 1.0),
+                        border_color: Color::ZERO,
+                        border_radius: Vec4::ZERO,
+                        border_width: Vec4::ZERO,
+                    },
+                ],
+            )
+        }
+    }
 }
