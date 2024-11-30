ddcc4cfcc7fc280d3f5109f55c77ad9e5410b783
Tomás Peyronel <tpeyronel@gmail.com>
2024-11-30 14:30:08 -0300

    add failing BorderThickness rounding test

--- src/ui.rs
+++ src/ui.rs
@@ -1807,6 +1807,39 @@ mod tests {
         use super::*;
 
         #[test]
+        fn border_thickness_rounding() {
+            test_converter(
+                32.0,
+                32.0,
+                UiNode::Box(BoxProps {
+                    modifiers: Modifiers::new().border_thickness(BorderThickness::all(3.5)), // Should all be rounded to 4.0
+                    children: vec![UiNode::Box(BoxProps {
+                        modifiers: Modifiers::new().fill_color(Color::new(1.0, 0.0, 0.0, 1.0)),
+                        children: vec![],
+                    })],
+                }),
+                &[
+                    Rectangle {
+                        position: Vec2::new(0.0, 0.0),
+                        size: Vec2::new(32.0, 32.0),
+                        fill_color: Color::ZERO,
+                        border_color: Color::ZERO,
+                        border_radius: Vec4::ZERO,
+                        border_width: Vec4::splat(4.0),
+                    },
+                    Rectangle {
+                        position: Vec2::new(4.0, 4.0),
+                        size: Vec2::new(32.0 - 8.0, 32.0 - 8.0),
+                        fill_color: Color::new(1.0, 0.0, 0.0, 1.0),
+                        border_color: Color::ZERO,
+                        border_radius: Vec4::ZERO,
+                        border_width: Vec4::ZERO,
+                    },
+                ],
+            )
+        }
+
+        #[test]
         fn padding_rounding() {
             test_converter(
                 32.0,
