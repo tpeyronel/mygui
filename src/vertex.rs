6fcc5a0119554002389dc6c58948c878932a904a
Tomás Peyronel <tpeyronel@gmail.com>
2024-12-09 14:47:49 -0300

    add uv to vertex

--- src/vertex.rs
+++ src/vertex.rs
@@ -6,14 +6,22 @@ pub type Color = Vec4;
 #[derive(Clone, Copy, Debug, bytemuck::Pod, bytemuck::Zeroable)]
 pub struct Vertex {
     pub pos: Vec2,
+    pub uv: Vec2,
 }
 
 impl Vertex {
-    const VERTEX_ATTRIBUTES: &[wgpu::VertexAttribute] = &[wgpu::VertexAttribute {
-        format: wgpu::VertexFormat::Float32x2,
-        offset: 0,
-        shader_location: 0,
-    }];
+    const VERTEX_ATTRIBUTES: &[wgpu::VertexAttribute] = &[
+        wgpu::VertexAttribute {
+            format: wgpu::VertexFormat::Float32x2,
+            offset: 0,
+            shader_location: 0,
+        },
+        wgpu::VertexAttribute {
+            format: wgpu::VertexFormat::Float32x2,
+            offset: std::mem::size_of::<Vec2>() as u64,
+            shader_location: 1,
+        },
+    ];
 
     pub fn vertex_buffer_layout() -> wgpu::VertexBufferLayout<'static> {
         wgpu::VertexBufferLayout {
