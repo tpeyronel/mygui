d8521ef5874c4563e34044d4b06c8004c4b5091b
Tomás Peyronel <tpeyronel@gmail.com>
2025-01-10 01:11:51 -0300

    improve BorderRadius interface

--- src/ui/border_radius.rs
+++ src/ui/border_radius.rs
@@ -2,43 +2,50 @@ use glam::Vec4;
 
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub struct BorderRadius {
-    pub bottom_left: f32,
-    pub bottom_right: f32,
-    pub top_right: f32,
-    pub top_left: f32,
+    bottom_left: f32,
+    bottom_right: f32,
+    top_right: f32,
+    top_left: f32,
 }
 
 impl BorderRadius {
     pub fn new(bottom_left: f32, bottom_right: f32, top_right: f32, top_left: f32) -> Self {
         Self {
-            bottom_left,
-            bottom_right,
-            top_right,
-            top_left,
+            bottom_left: bottom_left.round(),
+            bottom_right: bottom_right.round(),
+            top_right: top_right.round(),
+            top_left: top_left.round(),
         }
     }
 
     pub fn all(x: f32) -> Self {
-        Self {
-            bottom_left: x,
-            bottom_right: x,
-            top_right: x,
-            top_left: x,
-        }
+        let x = x.round();
+        Self::new(x, x, x, x)
     }
 
     pub fn to_vec4(&self) -> Vec4 {
         Vec4::new(self.bottom_left, self.bottom_right, self.top_right, self.top_left)
     }
+
+    pub fn bottom_left(&self) -> f32 {
+        self.bottom_left
+    }
+
+    pub fn bottom_right(&self) -> f32 {
+        self.bottom_right
+    }
+
+    pub fn top_right(&self) -> f32 {
+        self.top_right
+    }
+
+    pub fn top_left(&self) -> f32 {
+        self.top_left
+    }
 }
 
 impl Default for BorderRadius {
     fn default() -> Self {
-        Self {
-            bottom_left: 0.0,
-            bottom_right: 0.0,
-            top_right: 0.0,
-            top_left: 0.0,
-        }
+        Self::all(0.0)
     }
 }
